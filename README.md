c4843f1b57970cf8e9e329061e5d668de3fb7b9d
Sheena Brevig <sheena.brevig@gmail.com>
2019-07-22 14:34:08 -0700

    friends db created, survey dropdowns working

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # FriendFinder
+-
+A simple compatability app, that based on the user's response will cross compare data in order to find their most compatible friend in the database. Uses Node.js, Express, and Path.
+
+#Table of Contents
+-
+
+* General info
+* Technologies
+* Setup
+* Status
+
--- server.js
+++ server.js
@@ -3,8 +3,13 @@ var path = require("path");
 
 //set up Express app
 var app = express();
-var PORT = process.env.PORT || 3001;
+var PORT = process.env.PORT || 4000;
 
 //sets up Express for data handling
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+    console.log("server listening on http://localhost:" + PORT);
+  });
